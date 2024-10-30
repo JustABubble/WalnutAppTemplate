9d160699a196606c9e5ad925b2cb2a6ceef51319
Matthew McElroy <matthew@justabubble.com>
2024-10-31 00:50:30 +1100

    Update template to dev branch of Walnut

--- premake5.lua
+++ premake5.lua
@@ -3,8 +3,12 @@ workspace "WalnutApp"
    architecture "x64"
    configurations { "Debug", "Release", "Dist" }
    startproject "WalnutApp"
+   
+   -- Workspace-wide build options for MSVC
+   filter "system:windows"
+      buildoptions { "/EHsc", "/Zc:preprocessor", "/Zc:__cplusplus" }
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
-include "Walnut/WalnutExternal.lua"
 
+include "Walnut/Build-Walnut-External.lua"
 include "WalnutApp"
